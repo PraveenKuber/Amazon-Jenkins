b0f0d1d9447a56a0bc2a0731a18b45bb21fe096d
Praveen <40660524+PraveenKuber@users.noreply.github.com>
2024-03-07 06:50:24 +0530

    test

---
+++ Amazon-Core/src/test/java/org/amazon/NewTest.java
@@ -0,0 +1,22 @@
+package org.amazon;
+
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.assertTrue;
+
+/*
+*
+* Linked List : 1,2,3,4,5
+* reverse: 5,4,3,2,1
+*
+* */
+
+
+public class NewTest {
+    
+        public static void main(String[] args) {
+
+        }
+}
