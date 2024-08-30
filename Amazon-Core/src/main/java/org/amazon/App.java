00da9e20cea4c415ffe7939ee2ebb52322b22f6b
amruthaharsh <amruthaharshini1105@gmail.com>
2024-08-30 22:25:58 +0530

    Update App.java

--- Amazon-Core/src/main/java/org/amazon/App.java
+++ Amazon-Core/src/main/java/org/amazon/App.java
@@ -1,12 +1,12 @@
-package org.amazon
+package org.amazon;
 
 /**
  * Hello world!
  */
 public class App 
 {
-    public static void main( String[] args 
+    public static void main( String[] args) 
     {
-        System.out.println( "Hello World!" 
+        System.out.println( "Hello World!" );
     }
 }
