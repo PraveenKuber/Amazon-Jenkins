dff33ea51a760687e657f627f7bd16b0f530b631
sk-shri <kshrilakshmi912@gmail.com>
2025-02-04 16:31:07 +0530

    Update App.java

--- Amazon-Core/src/main/java/org/amazon/App.java
+++ Amazon-Core/src/main/java/org/amazon/App.java
@@ -7,7 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" )
+        System.out.println( "Hello World!" );
         
     }
 }
